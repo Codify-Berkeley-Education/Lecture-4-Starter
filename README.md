749dd86007f2cd6467d6f1508e8820fe8296adab
aidansunbury <aidansunbury@gmail.com>
2023-09-19 16:16:27 -0700

    more work

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lecture-4-Starter
+
+This lecture uses the json-server npm library to host a simple server that supports read and write actions from a json file.
+
+To run the server with the current set of data run
+
+> npx json-server --watch db.json
---
+++ loadData.js
@@ -0,0 +1,38 @@
+// To replace the current database with a different one, run this file with Node.js
+
+const fs = require("fs");
+
+// To load a different JSON file, change the value of sourceFilePath to any of the files within the databases folder
+const sourceFilePath = "databases/full_db.json";
+const targetFilePath = "db.json";
+
+// Read the contents of the source JSON file
+fs.readFile(sourceFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`Error reading ${sourceFilePath}: ${err.message}`);
+    return;
+  }
+
+  try {
+    // Parse the JSON data from the source file
+    const jsonData = JSON.parse(data);
+
+    // Convert the JSON data back to a string with 2-space indentation (for formatting)
+    const jsonString = JSON.stringify(jsonData, null, 2);
+
+    // Write the JSON data to the target JSON file
+    fs.writeFile(targetFilePath, jsonString, "utf8", (err) => {
+      if (err) {
+        console.error(`Error writing to ${targetFilePath}: ${err.message}`);
+      } else {
+        console.log(
+          `Contents of ${sourceFilePath} have been successfully copied to ${targetFilePath}`
+        );
+      }
+    });
+  } catch (parseError) {
+    console.error(
+      `Error parsing JSON data from ${sourceFilePath}: ${parseError.message}`
+    );
+  }
+});
